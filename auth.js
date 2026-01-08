10934d31933ea4b3d702f4e03caa010db31bf7f5
karthik12794 <kandimallakarthik4@gmail.com>
2026-01-08 23:48:06 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,17 +1,19 @@
-function sendOTP() {
+function sendOTP(){
+  if(!document.getElementById("phone").value){ alert("Enter phone number"); return;}
+  document.getElementById("phoneBox").classList.add("hidden");
   document.getElementById("otpBox").classList.remove("hidden");
 }
 
-function verifyOTP() {
+function verifyOTP(){
+  if(!document.getElementById("otp").value){ alert("Enter OTP"); return;}
+  document.getElementById("otpBox").classList.add("hidden");
   document.getElementById("pinBox").classList.remove("hidden");
 }
 
-function setPIN() {
-  localStorage.setItem("login", "true");
-  location.href = "home.html";
+function setPIN(){
+  if(!document.getElementById("pin").value){ alert("Enter PIN"); return;}
+  localStorage.setItem("login","true");
+  location.href="home.html";
 }
 
-function logout() {
-  localStorage.clear();
-  location.href = "index.html";
-}
+function logout(){localStorage.clear(); location.href="index.html";}
