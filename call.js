2ffcd97dec07603aeb4c9a402880f474944bacfb
karthik12794 <kandimallakarthik4@gmail.com>
2026-01-08 23:49:05 +0530

    Update call.js

--- call.js
+++ call.js
@@ -1,7 +1,9 @@
-function requestCall() {
-  document.getElementById("status").innerText = "Request Sent...";
+function requestCall(){
+  const recipient=document.getElementById("recipient").value;
+  const type=document.getElementById("callType").value;
+  const status=document.getElementById("status");
+  status.innerText="Request Sent...";
   setTimeout(()=>{
-    document.getElementById("status").innerText =
-      Math.random() > 0.5 ? "Call Accepted" : "Call Rejected";
+    status.innerText=Math.random()>0.5?`${type} with ${recipient} Accepted`:`${type} with ${recipient} Rejected`;
   },2000);
 }
